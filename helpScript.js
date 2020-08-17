4170edef7b55ad53d66ebcd93bf1fac3e9af6f3e
Tanvir <tanvirrahman0047@gmail.com>
2020-08-17 18:07:44 +0600

    modified

--- helpScript.js
+++ helpScript.js
@@ -1,20 +1,20 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name')
-var temp = document.querySelector('.temp')
-var lead = document.querySelector('.lead')
-var icon = document.querySelector('.ico')
+let button = document.querySelector('.button')
+let inputValue = document.querySelector('.inputValue')
+let name = document.querySelector('.name')
+let temp = document.querySelector('.temp')
+let lead = document.querySelector('.lead')
+let icon = document.querySelector('.ico')
 
 button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=f2adf456f891e7a9617f32903727cf9f')
 .then(response => response.json())
 .then(data => {
-    var nameValue = data['name'];
-    var disIcon = data['weather'][0]['icon'];
-    var tempValue = data['main']['temp'];
-    var celsius = tempValue-273.15;
-    var celsiusRound = Math.round(celsius);
-    var leadValue = data['weather'][0]['description'];
+    let nameValue = data['name'];
+    let disIcon = data['weather'][0]['icon'];
+    let tempValue = data['main']['temp'];
+    let celsius = tempValue-273.15;
+    let celsiusRound = Math.round(celsius);
+    let leadValue = data['weather'][0]['description'];
     document.querySelector('.name').innerHTML = nameValue;
     temp.innerHTML = celsiusRound;
     lead.innerHTML = leadValue;
--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
-var inputValue = document.querySelector('.inputValue');
-var button = document.querySelector('.button');
-//var titleOne = document.querySelector('.title-one');
-var divArea = document.querySelector('name');
+let inputValue = document.querySelector('.inputValue');
+let button = document.querySelector('.button');
+//let titleOne = document.querySelector('.title-one');
+let diletea = document.querySelector('name');
 
 button.addEventListener('click', function(){
     fetch('https://api.lyrics.ovh/suggest/'+inputValue.value)
@@ -9,72 +9,228 @@ button.addEventListener('click', function(){
 .then(data =>{
     
     console.log(data);
-    var songTitle = data.data.map(inputValue => inputValue.title);
-    var songAlbum = data.data.map(songTitle => songTitle.album.title);
-    console.log(songAlbum);
-    var titleOne = songTitle[0];
-    var albumNameOne = songAlbum[0];
+    let detailsArea = document.getElementById('songArea')
+    detailsArea.style.display = "block";
+    let songTitle = data.data.map(inputValue => inputValue.title);
+    let songAlbum = data.data.map(songTitle => songTitle.album.title);
+    let artistName = data.data.map(songAlbum => songAlbum.artist.name);
+    console.log(artistName);
+    // let artName = artistName[0];
+    // console.log(artName);
+
+    let titleOne = songTitle[0];
+    let artistNameOne = artistName[0];
     document.getElementById('one').innerHTML=titleOne;
-    document.getElementById('albumOne').innerHTML=albumNameOne;
+    document.getElementById('artistOne').innerHTML=artistNameOne;
 
-    var titleTwo = songTitle[1];
-    var albumNameTwo = songAlbum[1];
+    let titleTwo = songTitle[1];
+    let artistNameTwo = artistName[1];
     document.getElementById('two').innerHTML=titleTwo;
-    document.getElementById('albumTwo').innerHTML=albumNameTwo;
+    document.getElementById('artistTwo').innerHTML=artistNameTwo;
 
-    var titleThree = songTitle[2];
-    var albumNameThree = songAlbum[2];
+    let titleThree = songTitle[2];
+    let artistNameThree = artistName[2];
     document.getElementById('three').innerHTML=titleThree;
-    document.getElementById('albumThree').innerHTML=albumNameThree;
+    document.getElementById('artistThree').innerHTML=artistNameThree;
 
-    var titleFour = songTitle[3];
-    var albumNameFour = songAlbum[3];
+    let titleFour = songTitle[3];
+    let artistNameFour = artistName[3];
     document.getElementById('four').innerHTML=titleFour;
-    document.getElementById('albumFour').innerHTML=albumNameFour;
+    document.getElementById('artistFour').innerHTML=artistNameFour;
 
-    var titleFive = songTitle[4];
-    var albumNameFive = songAlbum[4];
+    let titleFive = songTitle[4];
+    let artistNameFive = artistName[4];
     document.getElementById('five').innerHTML=titleFive;
-    document.getElementById('albumFive').innerHTML=albumNameFive;
+    document.getElementById('artistFive').innerHTML=artistNameFive;
 
-    var titleSix = songTitle[5];
-    var albumNameSix = songAlbum[5];
+    let titleSix = songTitle[5];
+    let albumNameSix = artistName[5];
     document.getElementById('six').innerHTML=titleSix;
-    document.getElementById('albumSix').innerHTML=albumNameSix;
+    document.getElementById('artistSix').innerHTML=albumNameSix;
 
-    var titleSeven = songTitle[6];
-    var albumNameSeven = songAlbum[6];
+    let titleSeven = songTitle[6];
+    let artistNameSeven = artistName[6];
     document.getElementById('seven').innerHTML=titleSeven;
-    document.getElementById('albumSeven').innerHTML=albumNameSeven;
+    document.getElementById('artistSeven').innerHTML=artistNameSeven;
 
-    var titleEight = songTitle[7];
-    var albumNameEight = songAlbum[7];
+    let titleEight = songTitle[7];
+    let artistNameEight = artistName[7];
     document.getElementById('eight').innerHTML=titleEight;
-    document.getElementById('albumEight').innerHTML=albumNameEight;
+    document.getElementById('artistEight').innerHTML=artistNameEight;
 
-    var titleNine = songTitle[8];
-    var albumNameNine = songAlbum[8];
+    let titleNine = songTitle[8];
+    let albumNameNine = artistName[8];
     document.getElementById('nine').innerHTML=titleNine;
-    document.getElementById('albumNine').innerHTML=albumNameNine;
+    document.getElementById('artistNine').innerHTML=albumNameNine;
 
-    var titleTen = songTitle[9];
-    var albumNameTen = songAlbum[9];
+    let titleTen = songTitle[9];
+    let artistNameTen = artistName[9];
     document.getElementById('ten').innerHTML=titleTen;
-    document.getElementById('albumTen').innerHTML=albumNameTen;
+    document.getElementById('artistTen').innerHTML=artistNameTen;
+
+  
+    //lyrics
+let songLyrics = document.getElementById('lyrics-area')
+let lyricBtn = document.getElementById('lyrics-btn');
+lyricBtn.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameOne+'/'+titleOne)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyrics = data.lyrics    
+        console.log(data)
+        document.getElementById('lyrics-area').innerHTML=fullLyrics
+            
+    })
+    .catch(err => alert("no lyrics found"))
+})
 
 
+let lyricBtnTwo = document.getElementById('lyrics-btn-two');
+lyricBtnTwo.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameTwo+'/'+titleTwo)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsTwo = data.lyrics
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsTwo
+            
+ 
+    })
+    .catch(err => alert("no lyrics found")) 
+})
+
+
+let lyricBtnThree = document.getElementById('lyrics-btn-three');
+lyricBtnThree.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameThree+'/'+titleThree)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsThree = data.lyrics
+        if(fullLyricsThree.error){
+            alert("error")
+        }
+        else{
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsThree 
+        }  
+                
     
+    })
+    
+})
+
+
+let lyricBtnFour = document.getElementById('lyrics-btn-four');
+lyricBtnFour.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameFour+'/'+titleFour)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsFour = data.lyrics
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsFour        
+    
+    })
+    .catch(err => alert("no lyrics found")) 
+})
+
+let lyricBtnFive = document.getElementById('lyrics-btn-five');
+lyricBtnFive.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameFive+'/'+titleFive)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsFive = data.lyrics
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsFive       
+    
+    })
+    .catch(err => alert("no lyrics found")) 
+})
+
+let lyricBtnSix = document.getElementById('lyrics-btn-six');
+lyricBtnSix.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameSix+'/'+titleSix)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsSix = data.lyrics
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsSix     
+    
+    })
+    .catch(err => alert("no lyrics found")) 
+})
+
+let lyricBtnSeven = document.getElementById('lyrics-btn-seven');
+lyricBtnSeven.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameSeven+'/'+titleSeven)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsSeven = data.lyrics
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsSeven   
+    
+    })
+    .catch(err => alert("no lyrics found")) 
+})
+
+let lyricBtnEight = document.getElementById('lyrics-btn-eight');
+lyricBtnEight.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameEight+'/'+titleEight)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsEight = data.lyrics
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsEight  
+    
+    })
+    .catch(err => alert("no lyrics found")) 
+})
+
+let lyricBtnNine = document.getElementById('lyrics-btn-nine');
+lyricBtnNine.addEventListener('click',function(){
+    songLyrics.style.display = "block"
+    fetch('https://api.lyrics.ovh/v1/'+artistNameNine+'/'+titleNine)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsNine = data.lyrics
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsNine
+    
+    })
+    .catch(err => alert("no lyrics found")) 
+})
+
+let lyricBtnTen = document.getElementById('lyrics-btn-ten');
+lyricBtnTen.addEventListener('click',function(){
+    fetch('https://api.lyrics.ovh/v1/'+artistNameTen+'/'+titleTen)
+    .then(response => response.json())
+    .then(data =>{
+        let fullLyricsTen = data.lyrics
+        console.log(data);
+        document.getElementById('lyrics-area').innerHTML=fullLyricsTen
+    
+    })
+    .catch(err => alert("no lyrics found")) 
+})
+
     
     // for(let i=0; i<10; i++){
-    //     var title = songTitle[i];
+    //     let title = songTitle[i];
     //     console.log(title);
     //     //titleOne.innerHTML = title;
     //     document.getElementById('one').innerHTML=title;
     // }
 
-    // var albumName = data.data.map(title => inputValue.album);
+    // let albumName = data.data.map(title => inputValue.album);
     // for(let i=0; i<10; i++){
-    //     var album = albumName[i];
+    //     let album = albumName[i];
     //     console.log(album);
     //     document.getElementById('albumOne').innerHTML=album;
     // }
@@ -83,3 +239,4 @@ button.addEventListener('click', function(){
     
 })
 })
+
